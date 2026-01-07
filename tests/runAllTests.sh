835bec335631c835bf76402870d60f4d8e8565fa
Thomas Pfeifer (qwe) <tpchuckles@gmail.com>
2026-01-06 22:12:48 -0500

    LACBED_iterative had wrong indexing for diff

--- tests/runAllTests.sh
+++ tests/runAllTests.sh
@@ -1,4 +1,4 @@
-scripts=( "00_probe.py" "01_potentials.py" "02_propagate_otf=False.py" "02_propagate_otf=True.py" "03_manyprobes.py" "04_haadf.py" "05_tacaw.py" "05_tacaw_chunkFFT.py" "05_tacaw_cropped.py" "06_loaders.py" "07_defocus.py" "08_LACBED_iterative.py" "08_LACBED_onthefly.py" "10_midgley.py" "11_SED.py" "12_aberrations.py" )
+scripts=(  "06_loaders.py" "07_defocus.py" "08_LACBED_iterative.py" "08_LACBED_onthefly.py" "10_midgley.py" "11_SED.py" "12_aberrations.py" )
 
 echo $(date) > runAllTests.log
 
