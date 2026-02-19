3d010f0da491bf35b8c360ee9743bfcb24a7e581
tpchuckles <tpchuckles@gmail.com>
2026-02-18 19:29:54 -0500

    runAllTests now uses separate folders to keep torchcpu and torchgpu from colliding on psi_data

--- tests/runAllTests.sh
+++ tests/runAllTests.sh
@@ -1,10 +1,37 @@
-scripts=( "00_probe.py" "01_potentials.py" "02_propagate_otf=False.py" "02_propagate_otf=True.py" "03_manyprobes.py" "04_haadf.py" "05_tacaw.py" "05_tacaw_chunkFFT.py" "05_tacaw_cropped.py" "06_loaders.py" "07_defocus.py" "08_LACBED_iterative.py" "08_LACBED_onthefly.py" "10_midgley.py" "11_SED.py" "12_aberrations.py" "21_memorytests.py" "04_haadf_otf.py" )
+scripts=( "00_probe.py"
+	"01_potentials.py"
+	"02_propagate_otf=False.py"
+	"02_propagate_otf=True.py"
+	"03_manyprobes.py"
+	"04_haadf.py"
+	"05_tacaw.py"
+	"05_tacaw_chunkFFT.py"
+	"05_tacaw_cropped.py"
+	"06_loaders.py"
+	"07_defocus.py"
+	"08_LACBED_iterative.py"
+	"08_LACBED_onthefly.py"
+	"10_midgley.py"
+	"11_SED.py"
+	"12_aberrations.py"
+	"21_memorytests.py"
+	"04_haadf_otf.py" )
 
 echo $(date) > runAllTests-$1.log
 
+mkdir ../$1
+
 for s in ${scripts[@]}
 do
-	echo python3 $s >> runAllTests-$1.log
-	python3 $s >> runAllTests-$1.log 2>> runAllTests-$1.log
-        #rm -rf psi_data/*
+	cp $s ../$1/
+	cd ../$1
+	ln -s ../tests/inputs inputs
+	ln -s ../tests/outputs outputs
+	echo python3 $s >> ../tests/runAllTests-$1.log
+	python3 $s >> ../tests/runAllTests-$1.log 2>> ../tests/runAllTests-$1.log
+        rm -rf psi_data/*
+	rm $s
+	cd ../tests
 done
+
+rmdir ../$1
